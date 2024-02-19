5e5ca585ac27336b7b85c1d9450950b4679985cd
Vinith1710 <vinith13579@gmail.com>
2024-02-19 15:23:57 +0530

    Complete for Pc

--- js/index.js
+++ js/index.js
@@ -1,58 +1,80 @@
 // First Page 
 function descriptionSubstring() {
     for (let i = 1; i < 4; i++) {
-        document.getElementById("description_" + i).innerHTML = document.getElementById("description_" + i).innerHTML.substring(0, 25) + "...";
+        let description = document.getElementById('product_description_' + i).innerHTML;
+        localStorage.setItem('productDescription_' + i, description + i);
+        document.getElementById("product_description_" + i).innerHTML = document.getElementById("product_description_" + i).innerHTML.substring(0, 40) + "...";
     }
-    //    var source = document.getElementById('content_list_1').getAttribute('src');
-    //    console.log("-->", source);
-
-    // var abc = document.getElementById('content_list_1').id;
-    // console.log('-->',abc);
 }
 
 function imageLoad(event) {
     var source = event.target.id;
-        localStorage.setItem('imageSource', source);
-        window.location.href = 'product.html';
-
-    // if (source == content_list_1) {
-    //     let i = 1;
-    //     localStorage.setItem('imageSource', i);
-    //     window.location.href = 'product.html';
-    // } 
-    // else if (source == content_list_2) {
-    //     let i = 2;
-    //     localStorage.setItem('imageSource', i);
-    //     window.location.href = 'product.html';
-    // }
-    // else {
-    //     let i = 3;
-    //     localStorage.setItem('imageSource', i);
-    //     window.location.href = 'product.html';
-    // }
-
+    localStorage.setItem('imageSource', source);
+    for (let i = 1; i < 4; i++) {
+        if (source == "content_list_" + i) {
+            let heading = document.getElementById('product_heading_' + i).innerHTML;
+            localStorage.setItem('productHeading', heading);
+        }
+    }
+    window.location.href = 'product.html';
 }
 
 // Second Page 
+
 function productPageLoad() {
-    let selectedProduced = localStorage.getItem('imageSource');
-    console.log(selectedProduced);
+    var selectedProduced = localStorage.getItem('imageSource');
+    if (selectedProduced == "content_list_1") {
+        let aDescription = localStorage.getItem('productDescription_1');
+        document.getElementById('product_description').innerHTML = aDescription;
+        document.getElementById('originalPrice').value = "45000";
+        for (let i = 1; i < 4; i++) {
+            document.getElementById('product_image_' + i).src = "images/PS5_" + i + ".jpg";
+        }
+    }
+    else if (selectedProduced == "content_list_2") {
+        let aDescription = localStorage.getItem('productDescription_2');
+        document.getElementById('product_description').innerHTML = aDescription;
+        document.getElementById('originalPrice').value = "30000";
+        for (let i = 1; i < 4; i++) {
+            document.getElementById('product_image_' + i).src = "images/NS_" + i + ".jpg";
+        }
+    }
+    else {
+        let aDescription = localStorage.getItem('productDescription_3');
+        document.getElementById('product_description').innerHTML = aDescription;
+        document.getElementById('originalPrice').value = "55000";
+        for (let i = 1; i < 4; i++) {
+            document.getElementById('product_image_' + i).src = "images/XB_" + i + ".jpg";
+        }
+    }
+    let aHeading = localStorage.getItem('productHeading');
+    document.getElementById('product_heading').innerHTML = aHeading;
 }
 
 function updateDiscount(event) {
-    if (event.target.value) {
-        document.getElementById('salePrice').value = 42750;
+    if (event.target.value == 1) {
+        let price = (document.getElementById('originalPrice').value * 90) / 100;
+        document.getElementById('salePrice').value = price;
+    }
+    else if (event.target.value == 2) {
+        let price = document.getElementById('originalPrice').value - 3500;
+        document.getElementById('salePrice').value = price;
     }
     else {
         document.getElementById('salePrice').value = null;
     }
 }
 
-
-
-function loadOrderPage() {
+function redirectOrderPage() {
     if (document.getElementById('salePrice').value) {
+        let orderImage = localStorage.getItem('imageSource');
+        localStorage.setItem('loadOrderImage', orderImage);
+        let aHeading = localStorage.getItem('productHeading');
+        localStorage.setItem('loadOrderHeading', aHeading);
+        let salePrice = document.getElementById('salePrice').value;
+        localStorage.setItem('loadSalePrice', salePrice);
         window.location.href = 'order.html';
+
     } else {
         document.getElementById('discount_id_show').style = "display: inline";
     }
@@ -60,12 +82,36 @@ function loadOrderPage() {
 
 
 
+// Last Page 
+function loadOrderPage() {
+    let orderHeading = localStorage.getItem('loadOrderHeading');
+    document.getElementById('order_heading').innerHTML = orderHeading;
+    let orderPrice = localStorage.getItem('loadSalePrice');
+    document.getElementById('orderSinglePrice').value = orderPrice;
+    document.getElementById('salePrice').value = orderPrice;
+
+    let orderImage = localStorage.getItem('loadOrderImage');
+    if (orderImage == "content_list_1") {
+        document.getElementById('order_page_image').src = "images/PS5_1.jpg";
+        document.getElementById('available_items').value = 17;
+    }
+    else if (orderImage == "content_list_2") {
+        document.getElementById('order_page_image').src = "images/NS_1.jpg";
+        document.getElementById('available_items').value = 8;
+    }
+    else {
+        document.getElementById('order_page_image').src = "images/XB_1.jpg";
+        document.getElementById('available_items').value = 24;
+    }
+}
 
 var no_of_order_start = 1;
 var no_of_order = document.getElementById('no_of_order_plus');
 no_of_order.addEventListener("click", function () {
-    if (no_of_order_start >= 1 && no_of_order_start < 10) {
-        document.getElementById('salePrice').value = 42750;
+    var singlePrice = localStorage.getItem('loadSalePrice');
+
+    if (no_of_order_start >= 1 && no_of_order_start < document.getElementById('available_items').value) {
+        document.getElementById('salePrice').value = singlePrice;
         no_of_order_start++;
         document.getElementById('salePrice').value = document.getElementById('salePrice').value * no_of_order_start;
     }
@@ -76,8 +122,9 @@ no_of_order.addEventListener("click", function () {
 
 var no_of_order = document.getElementById('no_of_order_minus');
 no_of_order.addEventListener("click", function () {
-    if (no_of_order_start > 1 && no_of_order_start <= 10) {
-        document.getElementById('salePrice').value = 42750;
+    var singlePrice = localStorage.getItem('loadSalePrice');
+    if (no_of_order_start > 1 && no_of_order_start <= document.getElementById('available_items').value) {
+        document.getElementById('salePrice').value = singlePrice;
         no_of_order_start--;
         document.getElementById('salePrice').value = document.getElementById('salePrice').value * no_of_order_start;
     }
@@ -85,7 +132,6 @@ no_of_order.addEventListener("click", function () {
     document.getElementById('no_of_order').value = no_of_order_start;
     console.log("---", document.getElementById('no_of_order').value);
 })
-
 function placeOrder() {
     document.getElementById('popup').style = "display: flex"
 }
